98a9ce180d6ef43b7891b7f7c3146ec0eabaa94a
Maurice <mauricenganga41@gmail.com>
2022-10-08 08:47:43 +0300

    Add review section in the prodcut page

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -77,6 +77,66 @@ export const Product = () => {
           </AddContainer>
         </InfoContainer>
       </Wrapper>
+      {/* <CommentContainer>
+        <CommentTitle>{product.reviews?.length} Reviews</CommentTitle>
+        {product.reviews?.map((review) => {
+          return (
+            <Comment key={review.id}>
+              <CommentHeader>
+                <UserName>{review.user.username}</UserName>
+                <CommentDate>
+                  {new window.Date(review.created_at).toDateString()}
+                  <CommentEdit>
+                    <Icon>
+                      <i
+                        className="singlePostIcon far fa-edit"
+                        onClick={() => setUpdateMode(!updateMode)}
+                      ></i>
+                    </Icon>
+                    <Icon>
+                      <i
+                        className="singlePostIcon far fa-trash-alt"
+                        onClick={() => handleDelete(review.id)}
+                      ></i>
+                    </Icon>
+                  </CommentEdit>
+                </CommentDate>
+              </CommentHeader>
+              <Post>{review.reviews}</Post>
+            </Comment>
+          );
+        })}
+      </CommentContainer>
+      <FormTitle>Write a review</FormTitle>
+      {user ? (
+        <ComposeForm onSubmit={handleSubmit}>
+          <FormWrapper>
+            <FormDesc>
+              Your email address will not be publish.Required fields are marked
+              *
+            </FormDesc>
+            <FormLabel htmlFor="content">Comment *</FormLabel>
+            <Content
+              id="content"
+              name="reviews"
+              type="text"
+              onChange={handleChange}
+            />
+            <FormLabel htmlFor="score">Score *</FormLabel>
+            <CommentScore
+              id="content"
+              name="score"
+              type="number"
+              onChange={handleChange}
+            />
+            <Button>Post Comment</Button>
+          </FormWrapper>
+        </ComposeForm>
+      ) : (
+        <Link to={"/register"}>
+          <Button style={{ marginBottom: "5vh" }}>Subscribe</Button>
+        </Link>
+      )} */}
       <Footer />
     </Container>
   );
@@ -131,6 +191,7 @@ const AddContainer = styled.div`
   width: 50%;
   align-items: center;
   justify-content: space-between;
+  margin-top: 1rem;
   ${mobile({ width: "100%" })};
 `;
 
@@ -162,3 +223,98 @@ const Button = styled.button`
     background-color: #f8f4f4;
   }
 `;
+
+const CommentContainer = styled.section``;
+
+const Comment = styled.article`
+  border-radius: 0.1rem;
+  box-sizing: border-box;
+  padding: 0.5rem 1rem;
+  margin-bottom: 0.5rem;
+  border-bottom: 1px solid lightgray;
+`;
+
+const CommentHeader = styled.article`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 0.5rem;
+  border-top-left-radius: 0.1rem;
+  border-top-right-radius: 0.1rem;
+`;
+
+const CommentDate = styled.span`
+  font-size: 0.9rem;
+  float: right;
+`;
+
+const CommentTitle = styled.h3``;
+
+const Post = styled.span`
+  font-size: 0.8rem;
+`;
+
+const UserName = styled.span`
+  font-size: 0.9rem;
+`;
+
+const ComposeForm = styled.form`
+  background-image: -webkit-linear-gradient(
+    45deg,
+    rgb(200, 25, 38) 30%,
+    rgb(239, 110, 27) 70%
+  );
+  border-radius: 0.3rem;
+  box-sizing: border-box;
+  margin-bottom: 2rem;
+  padding: 1rem;
+  color: #ffffff;
+  font-weight: 600;
+`;
+
+const FormWrapper = styled.article`
+  display: flex;
+  flex-direction: column;
+`;
+
+const FormTitle = styled.h2`
+  font-size: 1.2rem;
+  margin-top: 5vh;
+`;
+
+const FormDesc = styled.p`
+  font-size: 0.8rem;
+  margin-bottom: 0.5rem;
+`;
+
+const FormLabel = styled.label``;
+
+const Content = styled.textarea`
+  font-size: 1.25rem;
+  height: 10vh;
+  padding: 1.25rem;
+  color: #000000;
+  border: none;
+  margin-bottom: 1rem;
+  border-radius: 0.2rem;
+  &:focus {
+    outline: none;
+  }
+`;
+
+const CommentScore = styled.input`
+  width: 5rem;
+  padding: 1rem;
+  color: #000000;
+`;
+
+// const Button = styled.button`
+//   width: 10rem;
+//   margin-top: 1rem;
+//   border: none;
+//   alighn-self: end;
+//   padding: 1rem;
+//   background-color: #000000;
+//   color: #ffffff;
+//   font-weight: 600;
+//   cursor: pointer;
+// `;
