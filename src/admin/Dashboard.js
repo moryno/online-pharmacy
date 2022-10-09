d4e906cfdf19b811893e8ec2512038afbe6bd92c
Maurice <mauricenganga41@gmail.com>
2022-10-09 11:37:01 +0300

    Add a table in the table component to display the latest transcation happening in the site in the admin page

--- src/admin/Dashboard.js
+++ src/admin/Dashboard.js
@@ -4,6 +4,7 @@ import Chart from "../components/Chart";
 import DashboardNavbar from "../components/DashboardNavbar";
 import Featured from "../components/Featured";
 import Sidebar from "../components/Sidebar";
+import Table from "../components/Table";
 import Widget from "../components/Widget";
 
 const Dashboard = () => {
@@ -22,6 +23,10 @@ const Dashboard = () => {
           <Featured />
           <Chart />
         </Charts>
+        <ListContainer>
+          <ListTitle>Latest Transactions</ListTitle>
+          <Table />
+        </ListContainer>
       </HomeContainer>
     </Container>
   );
@@ -48,4 +53,17 @@ const Charts = styled.section`
   display: flex;
   padding: 0.3125rem 1.25rem;
   gap: 1.25rem;
+`;
+
+const ListContainer = styled.section`
+  box-shadow: 2px 4px 10px 1px rgba(0, 0, 0, 0.47);
+  -webkit-box-shadow: 2px 4px 10px 1px rgba(0, 0, 0, 0.47);
+  -moz-box-shadow: 2px 4px 10px 1px rgba(0, 0, 0, 0.47);
+  padding: 1.25rem;
+  margin: 1.25rem;
+`;
+
+const ListTitle = styled.div`
+  font-weight: 500;
+  margin-bottom: 1rem;
 `;
---
+++ src/components/Table.js
@@ -0,0 +1,93 @@
+import styled from "styled-components";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+
+const List = () => {
+  const rows = [
+    {
+      id: 1,
+      product: "Acer Nitro 5",
+      img: "https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc3/Acer_Nitro_5_India_Launch.png",
+      customer: "John Smith",
+      date: "1 MArch",
+      amount: 785,
+      method: "Cash on Delivery",
+      status: "Approved",
+    },
+    {
+      id: 2,
+      product: "Playstation 5",
+      img: "https://gmedia.playstation.com/is/image/SIEPDC/playstation-5-digital-edition-with-dualsense-front-product-shot-01-ps5-en-30jul20?$native--t$",
+      customer: "Susan Smith",
+      date: "2 MArch",
+      amount: 985,
+      method: "Mpesa",
+      status: "Pending",
+    },
+    {
+      id: 3,
+      product: "Playstation 5",
+      img: "https://cdn.vox-cdn.com/thumbor/cPeVH-m_b9pScRXzpCBW-nAHSfw=/0x0:2032x1355/1400x1400/filters:focal(1016x678:1017x679)/cdn.vox-cdn.com/uploads/chorus_asset/file/22863258/akrales_210917_4760_0175.jpg",
+      customer: "Laban",
+      date: "2 MArch",
+      amount: 1985,
+      method: "Cash",
+      status: "Approved",
+    },
+  ];
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Tracking ID</TableCell>
+            <TableCell>Product</TableCell>
+            <TableCell>Customer</TableCell>
+            <TableCell>Date</TableCell>
+            <TableCell>Amount</TableCell>
+            <TableCell>Payment Method</TableCell>
+            <TableCell>Status</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow key={row.id}>
+              <TableCell>{row.id}</TableCell>
+              <TableCell>
+                <RowWrapper>
+                  <Image src={row.img} alt="productImage" />
+                  {row.product}
+                </RowWrapper>
+              </TableCell>
+              <TableCell>{row.customer}</TableCell>
+              <TableCell>{row.date}</TableCell>
+              <TableCell>{row.amount}</TableCell>
+              <TableCell>{row.method}</TableCell>
+              <TableCell>{row.status}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default List;
+
+const RowWrapper = styled.article`
+  display: flex;
+  align-items: center;
+`;
+
+const Image = styled.img`
+  height: 2rem;
+  width: 2rem;
+  border-radius: 50%;
+  margin-right: 10px;
+  object-fit: cover;
+`;
