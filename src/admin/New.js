607bf2386ba7dcd5382390eb45b7bd6101fd168d
Maurice <mauricenganga41@gmail.com>
2022-10-09 15:43:37 +0300

    Create a form in the new page in the admin directory for creating a new user with the data that admin inputs

--- src/admin/New.js
+++ src/admin/New.js
@@ -1,7 +1,151 @@
-import React from "react";
+import { DriveFolderUploadOutlined } from "@mui/icons-material";
+import styled from "styled-components";
+import DashboardNavbar from "../components/DashboardNavbar";
+import Sidebar from "../components/Sidebar";
 
 const New = () => {
-  return <div>New</div>;
+  return (
+    <Container>
+      <Sidebar />
+      <NewContainer>
+        <DashboardNavbar />
+        <Top>
+          <Title>Add New User</Title>
+        </Top>
+        <Bottom>
+          <Left>
+            {" "}
+            <Image
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png"
+              alt="ItemImg"
+            />
+          </Left>
+          <Right>
+            <Form>
+              <FormInput>
+                <Label htmlFor="image">
+                  Image:{" "}
+                  <DriveFolderUploadOutlined style={{ cursor: "pointer" }} />
+                </Label>
+                <Input id="image" type="file" style={{ display: "none " }} />
+              </FormInput>
+              <FormInput>
+                <Label>Username</Label>
+                <Input type="text" placeholder="username" />
+              </FormInput>
+              <FormInput>
+                <Label>Full Name</Label>
+                <Input type="text" placeholder="full name" />
+              </FormInput>
+              <FormInput>
+                <Label>Email</Label>
+                <Input type="email" placeholder="email" />
+              </FormInput>
+              <FormInput>
+                <Label>Phone</Label>
+                <Input type="text" placeholder="phone" />
+              </FormInput>
+              <FormInput>
+                <Label>Password</Label>
+                <Input type="password" placeholder="password" />
+              </FormInput>
+              <FormInput>
+                <Label>Address</Label>
+                <Input type="text" placeholder="address" />
+              </FormInput>
+              <FormInput>
+                <Label>Street</Label>
+                <Input type="text" placeholder="street" />
+              </FormInput>
+              <Button>Send</Button>
+            </Form>
+          </Right>
+        </Bottom>
+      </NewContainer>
+    </Container>
+  );
 };
 
 export default New;
+
+const Container = styled.main`
+  display: flex;
+  color: #1e144f;
+`;
+
+const NewContainer = styled.section`
+  flex: 6;
+`;
+
+const Top = styled.section`
+  padding: 1.25rem;
+  margin: 1.25rem;
+  display: flex;
+  box-shadow: 2px 4px 10px 1px rgba(0, 0, 0, 0.47);
+  -webkit-box-shadow: 2px 4px 10px 1px rgba(0, 0, 0, 0.47);
+  -moz-box-shadow: 2px 4px 10px 1px rgba(0, 0, 0, 0.47);
+`;
+
+const Title = styled.h1`
+  color: lightgray;
+  font-size: 1.25rem;
+`;
+
+const Bottom = styled.section`
+  padding: 1.25rem;
+  margin: 1.25rem;
+  display: flex;
+  box-shadow: 2px 4px 10px 1px rgba(0, 0, 0, 0.47);
+  -webkit-box-shadow: 2px 4px 10px 1px rgba(0, 0, 0, 0.47);
+  -moz-box-shadow: 2px 4px 10px 1px rgba(0, 0, 0, 0.47);
+`;
+
+const Left = styled.article`
+  flex: 1;
+  text-align: center;
+`;
+
+const Image = styled.img`
+  height: 6.25rem;
+  width: 6.25rem;
+  border-radius: 50%;
+  object-fit: cover;
+`;
+
+const Right = styled.article`
+  flex: 2;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1.875rem;
+  justify-content: space-around;
+`;
+
+const FormInput = styled.article`
+  width: 40%;
+`;
+
+const Label = styled.label`
+  display: flex;
+  align-item: center;
+  gap: 10px;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  border: none;
+  padding: 5px;
+  border-bottom: 1px solid lightgray;
+`;
+
+const Button = styled.button`
+  width: 9.375rem;
+  padding: 10px;
+  border: none;
+  background-color: #1896ff;
+  color: #eff8ff;
+  cursor: pointer;
+  margin-top: 10px;
+`;
